6b3ea19c7c52d344ce475413318f1285f3cfecd2
jda2011 <jdaadj0228@gmail.com>
2026-09-14 20:12:28 +0900

    Add main banner and hero video fields to SiteConfig

--- my-project/models/SiteConfig.js
+++ my-project/models/SiteConfig.js
@@ -8,3 +8,26 @@ const siteConfigSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('SiteConfig', siteConfigSchema);
+
+const mongoose = require('mongoose');
+
+const siteConfigSchema = new mongoose.Schema({
+  mainBannerTitle: {
+    type: String,
+    default: '역사 동영상 도서관에 오신 것을 환영합니다'
+  },
+  mainBannerDescription: {
+    type: String,
+    default: '원하는 역사 동영상을 찾아보고 퀴즈를 풀어보세요.'
+  },
+  heroVideoUrl: {
+    type: String,
+    default: '' // 관리자가 직접 등록할 메인 대표 영상 URL
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('SiteConfig', siteConfigSchema);
