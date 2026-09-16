1519e02603684f9a17dfddafdfc6993bec80a884
jda2011 <jdaadj0228@gmail.com>
2026-09-16 20:39:23 +0900

    Update app.js

--- app.js
+++ app.js
@@ -90,3 +90,7 @@ app.use('/api/admin', adminRouter);
 app.use('/api/videos', videoRouter);
 
 module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
+});
