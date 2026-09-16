1f8797cc28419be04a39915e86d2397c64f321c9
jda2011 <jdaadj0228@gmail.com>
2026-09-16 19:05:23 +0900

    Add server listening on specified port

--- app.js
+++ app.js
@@ -32,3 +32,8 @@ app.use('/api/admin', adminRouter);
 app.use('/api/videos', videoRouter);
 
 module.exports = app;
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 서버가 ${PORT}번 포트에서 실행 중입니다.`);
+});
