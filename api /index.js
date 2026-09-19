9620e09ab5e848ffb9c91d530126346a1daa222d
jda2011 <jdaadj0228@gmail.com>
2026-09-19 11:28:26 +0900

    Clean up duplicate module exports in index.js
    Removed duplicate code for exporting app module.

--- api /index.js
+++ api /index.js
@@ -2,17 +2,3 @@
 const app = require('../app'); // 최상위 폴더에 있는 app.js를 불러옵니다.
 
 module.exports = app;
-
-// api/index.js
-const app = require('../app');
-
-module.exports = app;
-
-// api/index.js
-const app = require('../app');
-
-module.exports = app;
-
-const app = require('../app');
-
-module.exports = app;
